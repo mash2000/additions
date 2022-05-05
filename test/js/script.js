1aec912916d2b4339803e11b4e0b31c845b73cdf
kamshir <iammaksim2000shirshikov@gmail.com>
2022-05-05 23:28:43 +0300

    fetch data

--- test/js/script.js
+++ test/js/script.js
@@ -1,23 +1,37 @@
 window.addEventListener('DOMContentLoaded', () => {
 
   function req() {
-    const request = new XMLHttpRequest();
-    request.open("GET", 'http://localhost:3000/people');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    request.send();
-    request.addEventListener('load', function () {
-      if (request.status == 200) {
-        let data = JSON.parse(request.response);
-        createCards(data);
-      } else {
-        console.log("Something's going wrong!");
-      }
-    });
+    // const request = new XMLHttpRequest();
+    // request.open("GET", 'http://localhost:3000/people');
+    // request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    // request.send();
+    // request.addEventListener('load', function () {
+    //   if (request.status == 200) {
+    //     let data = JSON.parse(request.response);
+    //     createCards(data);
+    //   } else {
+    //     console.log("Something's going wrong!");
+    //   }
+    // });
+
+    getResource('http://localhost:3000/people')
+      .then(data => createCards(data))
+      .catch(err => console.log(err))
 
     this.remove();
   }
 
   document.querySelector('.app button').addEventListener('click', req, {"once": true});
+
+  async function getResource(url){
+    const res = await fetch(`${url}`);
+
+    if (!res.ok){
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  }
 
   function createCards(response) {
     response.forEach(item => {
